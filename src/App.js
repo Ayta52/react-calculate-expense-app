35a8ea027fb67720ad1947740f6738e510bf0fc4
Андрей Соколов <a.sokolov@onpoint.ru>
2022-11-28 16:25:39 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import CostItem from "./components/CostItem";
 
 function App() {
+
+  const costs = [
+    {
+      date: new Date(2022, 10, 28),
+      description: 'Холодильник',
+      amount: 999,
+    },
+    {
+      date: new Date(2022, 10, 28),
+      description: 'MacBook',
+      amount: 1254,
+    },
+    {
+      date: new Date(2022, 10, 28),
+      description: 'Jeans',
+      amount: 300,
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello world</h1>
+      <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount} />
+      <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount} />
+      <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount} />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/CostItem.js
@@ -0,0 +1,17 @@
+import './CostItem.css'
+import CostDate from './CostDate';
+
+
+function CostItem(props) {
+
+  return (
+    <div className='cost-item'>
+      <CostDate date={props.date} />
+      <div className='cost-item__description'>
+        <h2>{props.description}</h2>
+        <div className='cost-item__price'>${props.amount}</div>
+      </div>
+    </div>
+  )
+}
+export default CostItem;
